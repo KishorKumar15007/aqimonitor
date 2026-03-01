02e8d89e5e919c946b8aedd8248064e427e46847
SAUBHGYA7 <88571622+SAUBHGYA7@users.noreply.github.com>
2026-03-01 11:08:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "REMOVED",
+  apiKey: "API_KEY",
   databaseURL: "https://aqi-monitoring-sytem-default-rtdb.asia-southeast1.firebasedatabase.app",
 };
 
